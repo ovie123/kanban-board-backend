1803e008e60ef5f767d0438808645152ca7435da
ovie <ernyovie@gmail.com>
2022-02-18 08:53:39 +0100

    adding dist to gitignore

--- dist/config/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose = require("mongoose");
-const mongoUrl = process.env["MONGO_URL"] || "mongodb://localhost:27017/taskDb";
-mongoose
-    .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("mongodb connected"))
-    .catch((err) => console.log(err));
-exports.default = mongoose;
